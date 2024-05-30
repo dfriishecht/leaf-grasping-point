21ae9fa54ac7a9cf3826b7c713e3ad3eb7d54c71
dfriishecht <dfriishecht@olin.edu>
2024-05-30 16:32:10 -0400

    Update PointCloudMask.py

--- PCDandDepth/PointCloudMask.py
+++ PCDandDepth/PointCloudMask.py
@@ -8,16 +8,15 @@ from matplotlib import pyplot as plt
 from PIL import Image, ImageFilter
 
 # Load pointcloud from file and save its point position and original color as two numpy arrays
-pcd_path = "Pointclouds/demo_ (1).pcd"
+pcd_path = "PCDandDepth/Pointclouds/demo_ (1).pcd"
 pcd_load = o3d.io.read_point_cloud(pcd_path, format='pcd')
 pcd_points = np.asarray(pcd_load.points)
-pcd_colors_orig = np.asarray(pcd_load.colors)
-pcd_count = len(pcd_load.points)
-
-print(f"Point cloud with {pcd_count} points loaded!")
+pcd_colors = np.asarray(pcd_load.colors)
+fill = np.zeros((1555200-len(pcd_colors), 3))
+pcd_colors = np.concatenate((pcd_colors, fill), axis=0)
 
 # Load mask to be used for leaf segmentation
-mask_image = Image.open('Images/aggrigated_masks (1).png')
+mask_image = Image.open('PCDandDepth/Images/aggrigated_masks (1).png')
 mask_array = np.asarray(mask_image)[:, :, 0:3]
 
 # Format mask array to be compatible with o3d's color array setup
@@ -31,66 +30,80 @@ mask[index] = [0,0,0]
 
 # Erode mask to remove artifacts in pointcloud from the depth map.
 mask = np.reshape(mask, (1080, 1440, 3))
-mask_erode = Image.fromarray(mask).filter(ImageFilter.MinFilter(7))
+mask_erode = Image.fromarray(mask).filter(ImageFilter.MinFilter(9))
 mask_erode = np.asarray(mask_erode)[:, :, 0:3]
 
 # Use leaf mask to remove all non-leaf portions of the pointcloud
 mask_erode = np.reshape(mask_erode, (1555200, 3))
-pcd_colors = np.asarray(pcd_load.colors)
-index = np.argwhere(mask_erode==[0, 0, 0])
-pcd_colors = mask_erode/255
-pcd_colors = np.delete(pcd_colors, index, 0)
-pcd_points = np.delete(pcd_points, index, 0)
-pcd_colors_orig = np.delete(pcd_colors_orig, index, 0)
+pcd_colors = pcd_colors * mask_erode
 
-mask_pcd_count = len(pcd_points)
+depth = np.load("PCDandDepth/Arrays/depth (1).npy")
+depth_load = o3d.io.read_point_cloud(pcd_path, format='pcd')
+depth_points = np.concatenate((np.asarray(depth_load.points), fill), axis=0)
+depth = depth_points[:,2]
+depth = np.reshape(depth, (1080, 1440, 1))
 
-print(f"Mask applied with {pcd_count - mask_pcd_count} points culled!")
-print(f"Current point count: {mask_pcd_count}")
+mask_gray = np.mean(mask_erode, -1)
+index = np.argwhere(mask_gray!=0)
+mask_gray[index] = 1
+mask_gray = np.reshape(mask_gray, (1080, 1440, 1))
+depth_masked = depth * mask_gray
 
-# Save the cropped leaves as new point cloud.
-pcd = o3d.geometry.PointCloud()
-pcd.colors = o3d.utility.Vector3dVector(pcd_colors)
-pcd.points = o3d.utility.Vector3dVector(pcd_points)
+xy_pos = np.asarray(depth_points)[:,0:2]
+xy_pos = np.reshape(xy_pos, (1080,1440, 2))
+xy_pos_masked = mask_gray * xy_pos
 
-# Remove final artifacts.
-# Prefer this over a more aggressive erosion since eventually it degrades the overall leaf shape
-cl, ind = pcd.remove_statistical_outlier(nb_neighbors=100, std_ratio=1.0)
-inlier_cloud = pcd.select_by_index(ind)
-outlier_cloud = pcd.select_by_index(ind, invert=True)
-outlier_cloud.paint_uniform_color([1, 0, 0])
-print(f"{len(outlier_cloud.points)} outlier points culled!")
-print(f"Current point count: {len(inlier_cloud.points)}")
-cropped_mask = np.asarray(inlier_cloud.colors)
-cropped_points = np.asarray(inlier_cloud.points)
+mask_colors = np.unique(mask_erode, axis=0)
+color_index = np.zeros(shape=(1555200, 1))
 
-# Generate a series of individual point clouds for each leaf and save them to a list
-mask_colors = np.unique(cropped_mask, axis=0)
-leaf_list = []
+i = 0
 for color in mask_colors:
-    index = np.argwhere(cropped_mask!=color)
-    colors = np.delete(cropped_mask, index, 0)
-    points = np.delete(cropped_points, index, 0)
-    pcd_single = o3d.geometry.PointCloud()
-    pcd_single.colors = o3d.utility.Vector3dVector(colors)
-    pcd_single.points = o3d.utility.Vector3dVector(points)
-    if len(points) > 100:
-        leaf_list.append(pcd_single)
-# Also store the complete set of leaves as one array
-all_leafs = cropped_points
+    index = np.argwhere(mask_erode==color)
+    color_index[index] = i
+    i+=1
 
+color_index = np.reshape(color_index, (1080, 1440, 1))
+masked_points = np.concatenate((xy_pos_masked,depth_masked,color_index), axis=2)
 
-# Create a plot of the final depth mask
-min_depth = np.min(all_leafs[:,2])
-max_depth = np.max(all_leafs[:,2])
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-depth_norm = (((all_leafs[:, 2]-min_depth) / (max_depth-min_depth)))
-plt.scatter(all_leafs[:,0], all_leafs[:,1], c=depth_norm,cmap=plt.cm.autumn_r, s=depth_norm)
-plt.gca().invert_yaxis()
-plt.colorbar()
-plt.title("Cropped Leaves Visualized w/ Normalized Depth")
-plt.xlabel("X-Position (m)")
-plt.ylabel("Y-Position (m)")
+left_image = Image.open("PCDandDepth/Images/left_rect (1).png")
+left_array = np.asarray(left_image)[:, :, 0:3]
+mask_gray_3d = np.repeat(mask_gray, 3, axis=2)
+left_array_masked = mask_gray_3d * (left_array/255)
+
+fig, ax = plt.subplot_mosaic([
+    ['x', 'y', 'z'],[ 'mask','orig', 'crop']
+], figsize=(15,10))
+
+ax['x'].imshow(masked_points[:, :, 0])
+ax['x'].set_title("X Coords (m)")
+ax['y'].imshow(masked_points[:, :, 1])
+ax['y'].set_title("Y Coords (m)")
+ax['z'].imshow(masked_points[:, :, 2])
+ax['z'].set_title("Depth (m)")
+ax['mask'].imshow(masked_points[:, :, 3])
+ax['mask'].set_title("Leaf ID")
+ax['orig'].imshow(left_image)
+ax['orig'].set_title("Original Image")
+ax['crop'].imshow(left_array_masked)
+ax['crop'].set_title("Cropped Image")
 plt.show()
+
+pcd = o3d.geometry.PointCloud()
+points = np.reshape(masked_points[:,:,0:3], (1555200, 3))
+points = np.delete(points, np.argwhere(points==[0,0,0]),axis=0)
+pcd.points = o3d.utility.Vector3dVector(points)
+
+
+# # Remove final artifacts.
+# # Prefer this over a more aggressive erosion since eventually it degrades the overall leaf shape
+# cl, ind = pcd.remove_statistical_outlier(nb_neighbors=100, std_ratio=1.0)
+# inlier_cloud = pcd.select_by_index(ind)
+# outlier_cloud = pcd.select_by_index(ind, invert=True)
+# outlier_cloud.paint_uniform_color([1, 0, 0])
+# print(f"{len(outlier_cloud.points)} outlier points culled!")
+# print(f"Current point count: {len(inlier_cloud.points)}")
+# cropped_mask = np.asarray(inlier_cloud.colors)
+# cropped_points = np.asarray(inlier_cloud.points)
+
+
 
